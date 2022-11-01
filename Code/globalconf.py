391f3a48e8197ef5201dc203a3caac9e08ef9bd9
aahmadian-liu <66072422+aahmadian-liu@users.noreply.github.com>
2022-11-01 14:55:00 +0100

    Update globalconf.py

--- Code/globalconf.py
+++ Code/globalconf.py
@@ -1,9 +1,11 @@
+# Basic Global Configurations
+
 gpu_mode=True
 visualize=False
-save_every_iters=0
-display_every_iters=3
 work_dir='./outputs' #directory for saving results
 data_dir='./data' #directory for downloading datasets
+num_workers=4
+save_every_iters=0
+display_every_iters=3
 save_on_epochs=[]
 show_data_mean=False
-num_workers=4
